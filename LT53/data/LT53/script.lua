4398fc9f8dbb3e70b50e29ef0f0dda2070690ff3
wieder-fi <wieder-fi@users.noreply.github.com>
2020-05-27 00:10:58 +0300

    LT53: fixed issues with getting the base ruleset from old version

--- LT53/data/LT53/script.lua
+++ LT53/data/LT53/script.lua
@@ -23,3 +23,42 @@ function city_destroyed_callback(city, loser, destroyer)
 end
 
 signal.connect("city_destroyed", "city_destroyed_callback")
+
+-- Kill triremes too far away from the coast
+function kill_triremes(turn, year)
+  local messages = {
+    'The crew of your Trireme has rebelled due to low food supply',
+    'Your Trireme was hit by scurvy: no survivors',
+    'Your Trireme encountered heavy sea and was lost body and property',
+    'Trireme lost to an attack by a giant squid',
+    'No news from your Trireme: presumed lost',
+  }
+  for tile in whole_map_iterate() do
+    if tile.terrain:class_name() == 'Oceanic' then
+      -- can have a trireme there
+      for unit in tile:units_iterate() do
+        if unit.utype:rule_name() == 'Trireme' then
+	  -- found one! Is the coast close enough?
+	  local at_coast = false
+	  for tile2 in tile:square_iterate(3) do
+	    -- look up to 3 tiles away
+            if tile2.terrain:class_name() ~= 'Oceanic' then
+	      at_coast = true
+              break
+	    end
+	  end
+	  -- too far, sorry...
+	  if not at_coast then
+	    notify.event(player, tile, E.UNIT_LOST_MISC,
+	                 messages[random(1, #messages)])
+	    unit:kill('fuel')
+	  end
+        end
+      end
+    end
+  end
+  -- continue processing
+  return false
+end
+
+signal.connect("turn_started", "kill_triremes")
