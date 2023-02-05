4e43facccb345ae5719c127c3e6d45aed6487689
Pranav <pranav.sampathkumar@gmail.com>
2023-02-05 21:45:11 +0100

    LTeX26TrueTurn: Change names to LTeX26TrueTurn

--- LTeX26TrueTurn/LT76Team.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-GAMEID=LT76Team
-GAMENAME=LT76Team
-GAMEDIR=$HOME/server/LT76Team
-PORT=5076
-
-. ../rc.subr.fc21
---
+++ LTeX26TrueTurn/LTeX26TrueTurn.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+GAMEID=LTeX26TrueTurn
+GAMENAME=LTeX26TrueTurn
+GAMEDIR=$HOME/server/LTeX26TrueTurn
+PORT=5076
+
+. ../rc.subr.fc21
